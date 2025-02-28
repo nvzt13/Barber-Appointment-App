cfeaa0b46aeffcce267a9b4d502fef69f730ab8a
nevzat atalay <nevzatatalay79@gmail.com>
2025-02-28 06:36:14 +0300

    All routes combined

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,25 @@ import Header from "@/components/sections/Welcome";
 import Services from "@/components/sections/Services";
 import Why from "@/components/sections/Why";
 
+
 export default function Home() {
+ const getServerSideProps = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/barber/read/1");
+    if (res.ok) {
+      const data = await res.json();
+      console.log(data);
+    } else {
+      console.log(res)
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  return { props: {} }; // Eğer sayfaya veri geçirilecekse buraya props eklenmeli
+};
+const {res} = getServerSideProps()
+console.log(res)
   return (
     <div className="mx-auto">
       <Header />
